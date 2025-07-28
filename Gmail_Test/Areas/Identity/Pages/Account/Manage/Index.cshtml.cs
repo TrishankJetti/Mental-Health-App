dd8e80b777098ee73e1816329df1fa846ae15725
ac121430 <ac121430@avcol.school.nz>
2025-07-29 11:10:22 +1200

    Better comments for the image upload system.

--- Gmail_Test/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Gmail_Test/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -105,33 +105,32 @@ namespace MentalHealthApp.Areas.Identity.Pages.Account.Manage
                 user.LastName = Input.LastName;
             }
 
-            // Handle profile picture upload
-            // Handle profile picture upload
-            if (Input.PfpFile != null && Input.PfpFile.Length > 0)
+           //Handles Profile Picture upload from user.
+            if (Input.PfpFile != null && Input.PfpFile.Length > 0)  // If the pfpfile is not set to null the if statement runs.
             {
                 // Validate file type and size
-                var validExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" };
-                var extension = Path.GetExtension(Input.PfpFile.FileName).ToLower();
+                var validExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" }; //This validExtensions variable creates an array of acceptable file types that may be accepted for the pfpfile. 
+                var extension = Path.GetExtension(Input.PfpFile.FileName).ToLower(); // This variable, extension gathers the extension of the current PfpFile's File Name. This is used to compare the PfpFile's extension with the validExtensions that are listed within the array.
 
-                if (!validExtensions.Contains(extension))
+                if (!validExtensions.Contains(extension)) // If the file doesnt contain any of the valid extensions then the Program will display an error message bellow. 
                 {
                     ModelState.AddModelError("Input.PfpFile", "Only image files (JPG, PNG, GIF) are allowed.");
                     await LoadAsync(user);
                     return Page();
                 }
 
-                var uploadsFolder = Path.Combine(_environment.WebRootPath, "img", "pfps");
-                Directory.CreateDirectory(uploadsFolder); // Creates folder if doesn't exist
+                var uploadsFolder = Path.Combine(_environment.WebRootPath, "img", "pfps");  // This code here defines the location of the iamges once they are uploaded. THe location is the Pfps fodler, within the Img folder within the "wwwroot" folder. 
+                
 
-                var uniqueFileName = $"{Guid.NewGuid()}{extension}";
-                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                var uniqueFileName = $"{Guid.NewGuid()}{extension}";        // This variable here creates a new UniqueFileName for each upload given.
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName); // Stores the filepath of the file uploaded.
 
                 using (var fileStream = new FileStream(filePath, FileMode.Create))
                 {
                     await Input.PfpFile.CopyToAsync(fileStream);
                 }
 
-                // Delete old pic if exists
+                // Delete old pic if a current Pfp Exists currently. 
                 if (!string.IsNullOrEmpty(user.PfpName) &&
                     user.PfpName != "default-pfp.png" )
                    
